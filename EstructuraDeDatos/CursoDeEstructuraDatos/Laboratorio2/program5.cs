dd3c956770a74eb6b2ecdd50b56cdd38a0c693d2
semcham <137580427+semcham@users.noreply.github.com>
2024-04-26 12:28:40 -0500

    Update program5.cs
    cambios

--- EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio2/program5.cs
+++ EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio2/program5.cs
@@ -1,11 +1,4 @@
-﻿List<int> listaCuadrados = new List<int>();
-
-    for (int i = 1; i < 11; i++) listaCuadrados.Add(i * i);
-
-    foreach (var dato in listaCuadrados)
-    {
-        Console.WriteLine($"Elemento de lista: {dato}");
-    }
+﻿// lista de cuadrados
 List<int> listaCuadrados = new List<int>();
 
 for (int i = 1; i < 11; i++) listaCuadrados.Add(i * i);
