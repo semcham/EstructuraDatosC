b92f7d46faa36d1c22949ab88a577fc836da11e2
semcham <137580427+semcham@users.noreply.github.com>
2024-04-26 10:09:14 -0500

    Update program3.cs
    cambios

--- EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio2/program3.cs
+++ EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio2/program3.cs
@@ -1,2 +1,23 @@
 ﻿
-public class 
+public class Contador_De_Vocales
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un texto : ");
+        string texto = Console.ReadLine();
+        int contador = 0;
+        int letra ;
+        for (int i = 0; i < texto.Length; i++)
+
+        {
+            letra = texto[i];
+            if ((letra =='a') || (letra == 'e') || (letra == 'i') || (letra == 'o') || (letra == 'u'))
+            {
+                contador ++;
+            }
+        }
+        Console.WriteLine("EL numero de vocales es :" + contador);
+
+    }
+
+}
