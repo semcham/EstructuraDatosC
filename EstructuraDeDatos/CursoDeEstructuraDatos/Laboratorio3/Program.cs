af60f7adeb04903abae6a249fbeaadde8d01ce48
semcham <137580427+semcham@users.noreply.github.com>
2024-04-26 15:43:43 -0500

    cambios

--- EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio3/Program.cs
+++ EstructuraDeDatos/CursoDeEstructuraDatos/Laboratorio3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double d, v, t;
+Console.Write("Ingrese la velocidad constante: ");
+v = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el tiempo: ");
+t = double.Parse(Console.ReadLine());
+d = v * t;
+
+Console.WriteLine("La distancia es : " + d);
